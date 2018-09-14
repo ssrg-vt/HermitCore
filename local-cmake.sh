8b48a3def0a6a5facb58296551aa3afae8ad2429
Pierre Olivier <polivier@vt.edu>
2018-09-14 11:12:28 -0400

    Remove dependencies on old hermitcore debian packages in local-cmake.sh

--- local-cmake.sh
+++ local-cmake.sh
@@ -16,6 +16,8 @@ mkdir build-aarch64
 cd build-x86-64
 cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
 	-DCOMPILER_BIN_DIR=$INSTALL_PATH/x86_64-host/bin \
+	-DCMAKE_OBJCOPY=$INSTALL_PATH/x86_64-host/bin/x86_64-hermit-objcopy \
+	-DCMAKE_AR=$INSTALL_PATH/x86_64-host/bin/x86_64-hermit-ar \
 	-DHERMIT_PREFIX=$INSTALL_PATH/x86_64-host \
 	-DMIGRATION_LOG=1 \
 	-DKERNEL_DEBUG=1 \
@@ -25,6 +27,8 @@ cd -
 cd build-aarch64
 cmake -DHERMIT_ARCH=aarch64 \
 	-DTARGET_ARCH=aarch64-hermit \
+	-DCMAKE_OBJCOPY=$INSTALL_PATH/x86_64-host/bin/aarch64-hermit-objcopy \
+	-DCMAKE_AR=$INSTALL_PATH/x86_64-host/bin/aarch64-hermit-ar \
 	-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH \
 	-DCOMPILER_BIN_DIR=$INSTALL_PATH/x86_64-host/bin \
 	-DHERMIT_PREFIX=$INSTALL_PATH/x86_64-host \
